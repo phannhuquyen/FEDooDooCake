e229a9181f79c5d05375cc65617816ba13ddead8
phannhuquyen <phannhuquyen88@gmail.com>
2026-05-17 20:30:51 +0700

    update

--- src/components/login/LoginFooter.tsx
+++ src/components/login/LoginFooter.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const LoginFooter = () => {
+  const navigate = useNavigate();
   return (
     <div className="absolute bottom-8 lg:text-left">
       <p className="text-xs w-fit   ">
-        @2026 Tiệm bánh DooDooCake. All rights reserved.
+        @2026 Tiệm bánh{" "}
+        <span
+          className="text-highlight cursor-pointer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          DooDooCake
+        </span>
+        . All rights reserved.
       </p>
     </div>
   );
